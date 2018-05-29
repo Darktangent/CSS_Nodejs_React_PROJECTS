0ad6e524119551401b16e785e8322e4739961e57
Rohan Ganguly <rohan.ganguly@snhu.edu>
2018-05-29 08:22:19 +0000

    Expanding mini.js file in mini social project folder

--- mini_social_project/mini.js
+++ mini_social_project/mini.js
@@ -2,6 +2,18 @@ var database = [
     {
         username: "andrei",
         password:"password"
+    }, 
+    {
+        username: "sally",
+        password:"password"
+    },
+    {
+        username: "ingrid",
+        password:"password"
+    },
+    {
+        username: "esha",
+        password:"password"
     }
 
 
@@ -24,13 +36,83 @@ var newsFeed = [
 var userNamePrompt=prompt("Whats your username");
 var passwordPrompt=prompt("Whats your password");
 
-function signIn(user, pass) {
-    if (user===database[0].username &&
-         pass===database[0].password){
-         console.log(newsFeed)
-         }else {
-             alert("Sorry wrong username and password");
-         }
+// creating a new function to see if userValid
+
+function isValidUser(username,password){
+    for(var i=0; i<database.length; i++){
+        if (database[i].username==username &&
+            database[i].password==password){
+                return true;
+            }
+            } return false;
+    }
+    
+
+
+
+
+function signIn(username, password) {
+    
+    if (isValidUser(userNamePrompt,passwordPrompt)){
+        console.log(newsFeed);
+        alert (`Welcome to our social platform `+ userNamePrompt);
+
+    } else {
+        alert("Sorry, invalid username and password combination")
+    }
+    
+    // if (user===database[0].username &&
+    //      pass===database[0].password){
+    //      console.log(newsFeed)
+    //      }else {
+    //          alert("Sorry wrong username and password");
+    //      }
 }
 
 signIn(userNamePrompt,passwordPrompt)
+
+// var database = [
+//     {
+//         username: "andrei",
+//         password:"password"
+//     }
+
+
+// ];
+
+// var newsFeed = [
+
+//     {
+//         username:"Bobby",
+//         timeline:"So tired from all that learning"
+//     },
+//     {
+//         username:"Sally",
+//         timeline: "Javaswcript is so cool"
+//     }
+
+// ]
+
+    // if (user===database[0].username &&
+    //      pass===database[0].password){
+    //      console.log(newsFeed)
+    //      }else {
+    //          alert("Sorry wrong username and password");
+    //      }
+// }
+
+signIn(userNamePrompt,passwordPrompt)
+
+// var userNamePrompt=prompt("Whats your username");
+// var passwordPrompt=prompt("Whats your password");
+
+// function signIn(user, pass) {
+//     if (user===database[0].username &&
+//          pass===database[0].password){
+//          console.log(newsFeed)
+//          }else {
+//              alert("Sorry wrong username and password");
+//          }
+// }
+
+// signIn(userNamePrompt,passwordPrompt)
